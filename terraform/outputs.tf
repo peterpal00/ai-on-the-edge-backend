407cb82a0da521d1272ec04a76199e2811af26c7
K. Peti <pkovacs@cmtelematics.com>
2023-02-17 23:49:15 +0100

    fix(repo settings): terraform added

---
+++ terraform/outputs.tf
@@ -0,0 +1,4 @@
+output "repo_name" {
+  description = "The name of the repository"
+  value       = github_repository.ai-edge-backend.name
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "TF_TOKEN" {
+  type        = string
+  description = "Specifies the GitHub PAT token or `GITHUB_TOKEN`"
+  sensitive   = true
+}
